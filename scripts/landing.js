c809b50617c5b3fb3c353f200ad1a8cc35b3a521
Noel Worden <noelworden@gmail.com>
2016-11-01 07:13:50 -0600

    [FEAT] animation of points after scroll Only show elements once scrolled down to them or if screen is large enough to show on page load
    Closes #23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,11 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-//WORKING CODE
-    var points = document.getElementsByClassName('point');
+var animatePoints = function(points){
+
+        
+//var animatePoints = function() {
+//
+//    var points = document.getElementsByClassName('point');
     
     var revealPoint = function(index){
         points[index].style.opacity = 1;
@@ -14,47 +18,19 @@ var animatePoints = function() {
         revealPoint(i);
         
     };
+};
     
-// MY ATTEMPT
-//    
-//    var points = document.querySelectorAll('point');
-//    
-//    for(var i = 0; i < points.length; i++){
-//        point[i].style.opacity = 1;
-//        point[i].style.transform = "scaleX(1) translateY(0)";
-//        point[i].style.msTransform = "scaleX(1) translateY(0)";
-//        point[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-//    };
-//    
+window.onload = function(){
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
     
-// ORIGINAL
-//     var points = document.getElementsByClassName('point');
-// 
-//     var revealFirstPoint = function() {
-//         points[0].style.opacity = 1;
-//         points[0].style.transform = "scaleX(1) translateY(0)";
-//         points[0].style.msTransform = "scaleX(1) translateY(0)";
-//         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-//     };
-// 
-//     var revealSecondPoint = function() {
-//         points[1].style.opacity = 1;
-//         points[1].style.transform = "scaleX(1) translateY(0)";
-//         points[1].style.msTransform = "scaleX(1) translateY(0)";
-//         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-//     };
-// 
-//     var revealThirdPoint = function() {
-//         points[2].style.opacity = 1;
-//         points[2].style.transform = "scaleX(1) translateY(0)";
-//         points[2].style.msTransform = "scaleX(1) translateY(0)";
-//         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-//     };
-// 
-//     revealFirstPoint();
-//     revealSecondPoint();
-//     revealThirdPoint();
- 
- };
-
-//animatePoints();
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event){
+       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+           animatePoints(pointsArray);
+       }                        
+    });
+}
