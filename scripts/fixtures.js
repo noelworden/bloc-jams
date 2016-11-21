9f03f63b85aedd7aa820d2ed54f29d22a887ee87
Noel Worden <noelworden@gmail.com>
2016-11-21 06:16:00 -0700

    [FEAT] JQ prev/next. Song displayed in bar Also added a folder of test files for next meeting.

---
+++ scripts/fixtures.js
@@ -0,0 +1,31 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1981',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Orange', duration: '3:24' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+
+//Another Example Album
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Gugliemlmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '4:26' },
+        { title: 'Ring ring', duration: '3:14' },
+        { title: 'Fits in your pocket', duration: '5:01' },
+        { title: 'Can you hear me now?', duration: '3:21'},
+        { title: 'Wrong number', duration: '2:15'}
+    ]
+};
